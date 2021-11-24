52f6e1d5d4b480591f5e637217d8261d4736fc4f
Derek H-M <derekcom16@gmail.com>
2021-11-23 17:42:54 -0800

    Commit calibration result data
    * Update SSTL Calibration test script to save the output in JSON format.
    * Commit the JSON calibration result for the 4 cases.

--- sim_sstl_res.py
+++ sim_sstl_res.py
@@ -3,6 +3,7 @@ from leg_sim_util import *
 from math import floor
 from numpy import arange
 from random import randrange
+import json
 
 def cal2ctrl(n):
   result = [0, 0, 0, 0]
@@ -11,7 +12,6 @@ def cal2ctrl(n):
   return result
 
 def simCal(template, ctrl_dict, temp, vdd, proc):
-  name = 'sstl_cal_sim_{t}_{v}_{p}'.format(t=temp, v=vdd, p=proc)
   num_pd_en = 0
   for i in range(7): 
     if ctrl_dict['pd'][i]['en'] == 1: num_pd_en += 1
@@ -23,6 +23,12 @@ def simCal(template, ctrl_dict, temp, vdd, proc):
     exit()
   is_pulldown = num_pd_en != 0
   expected_res = 240/max(num_pd_en, num_pu_en)
+  direction = 'pu'
+  if is_pulldown: direction = 'pd'
+
+  name = 'sstl_{d}_{n}_cal_sim_{t}_{v}_{p}'.format(
+    d=direction, n=max(num_pd_en, num_pu_en), t=temp, v=vdd, p=proc)
+
 
   search_max = 15
   search_min = 0
@@ -46,9 +52,8 @@ def simCal(template, ctrl_dict, temp, vdd, proc):
     
     if not(any(res)): # If all in range, we found calibration!
       print('Calibration found: {c} target resistance = {t} ohms.'.format(
-        c=ctrl_sig, t=round(expected_res, 2)))
-      #print('Resistances = {r}'.format(r=result))
-      return {'cal':ctrl_sig, 'res':result}
+        c=ctrl_sig, t=round(expected_res, 2)))      
+      return {'cal':cal, 'res':result}
     elif 1 in res and -1 in res: # If some out of range on either end, calibration is impossible!
       print('Calibration impossible with resistances: {r} target resistance = {t} ohms.'.format(
         r=result, t=round(expected_res, 2)))
@@ -60,14 +65,13 @@ def simCal(template, ctrl_dict, temp, vdd, proc):
     else: assert(False)
   assert(False, 'CALIBRATION FAILED!') 
 
-#template = 'sstl_res_tb.spice'
-# ctrl_dict = {'pu':7*[{'en':0, 'cal':[0,1,1,1]}],
-#              'pd':7*[{'en':0, 'cal':[0,0,1,1]}]}
-# for i in range(7): ctrl_dict['pd'][i]['en'] = 1
-# print('Settings: {}'.format(ctrl_dict))
-# result = sstl_res_sim(template, outName='sstl_sim_typ', temp=27, vddVoltage=1.5, 
-#       process='tt', ctrl_dict=ctrl_dict)
-# print('Result = {}'.format(result))
+
+
+
+
+
+
+
 
 tmp = 'sstl_res_tb.spice' # Spice script template
 # Default control signal set
@@ -84,16 +88,32 @@ else:
   print('Usage: {fname} pulldown/pullup <NUM_LEG_ENABLED_OPTION>'.format(fname=argv[0]))
   quit()
 
+with open('sstl_cal_log.txt', 'w') as outf: pass # Clear previous log
+cal_arr = []
 temps = [-40, 125] # Temperature edge cases
 vdds = [1.575, 1.475] # Edge cases of voltage (1.5V +/-5%)
 procs = ['ff', 'ff_mm', 'fs', 'fs_mm', 'hh', 'hh_mm', 'hl', 'hl_mm', 'lh', 'lh_mm', 'll', 'll_mm', 'sf', 'sf_mm', 'ss', 'ss_mm', 'tt', 'tt_mm']
-with open('sstl_cal_log.txt', 'w') as outf:
-  for temp in temps:
-    for vdd in vdds:
-      for proc in procs:
-        output = simCal(tmp, ctrl_dict, temp, vdd, proc)
+for temp in temps:
+  for vdd in vdds:
+    for proc in procs:
+      output = simCal(tmp, ctrl_dict, temp, vdd, proc)
+      # Save output
+      out_dict = {}
+      out_dict['temp'] = temp
+      out_dict['vdd'] = vdd
+      out_dict['process'] = proc
+      out_dict['valid_cal_enum'] = output['cal']
+      out_dict['calibrated_resistances'] = [round(output['res']['r_lvt'],1),
+                                            round(output['res']['r_mvt'],1),
+                                            round(output['res']['r_hvt'],1) ]
+      cal_arr.append(out_dict)
+
+      with open('sstl_cal_log.txt', 'a') as outf:
         outf.write('{t}, {v}, {p}\t\t{cal}\t{res}\n'.format(
           t=temp, v=vdd, p=proc, cal=output['cal'], res=output['res']))
-        print()
-        print()
+      print()
+      print()
+
+json_out_name = 'sstl_{t}_{r}_ohm_cal.json'.format(t=argv[1], r=round(240/float(argv[2])))
+with open(json_out_name, 'w') as f: f.write(json.dumps(cal_arr, indent=2))
 print('ALL CORNER CALIBRATION CASES PASSED!')
