50b4a09a26f67a4caf3011e68fa1bd03998ee8d4
Derek H-M <derekcom16@gmail.com>
2022-04-21 13:20:54 -0700

    Add capacitance measurement testbench draft
    * Add schematic and python script only

---
+++ schem/post_layout_sstl_cap_tb.sch
@@ -0,0 +1,94 @@
+v {xschem version=3.0.0 file_version=1.2 
+
+* Copyright 2021 Stefan Frederik Schippers
+* 
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     https://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+
+}
+G {}
+K {}
+V {}
+S {}
+E {}
+N 350 -360 380 -360 { lab=pu_cal[0:3]}
+N 350 -300 380 -300 { lab=pd_cal[0:3]}
+N 240 -270 380 -270 { lab=GND}
+N 240 -390 380 -390 { lab=GND}
+N 860 -330 920 -330 { lab=DQ}
+N 980 -330 1000 -330 { lab=VDDQ}
+C {devices/title.sym} 160 -30 0 0 {name=l1 author="Derek H-M"}
+C {devices/code.sym} 790 -200 0 0 {name=STIMULI 
+only_toplevel=true
+place=end
+value="
+* power voltage
+vvdd VDD 0 SED_vdd_SED
+
+** CALIBRATION CONTROL **
+* PULLUP
+vpu_cal0 pu_cal[0] 0 SED_pucal0_SED
+vpu_cal1 pu_cal[1] 0 SED_pucal1_SED
+vpu_cal2 pu_cal[2] 0 SED_pucal2_SED
+vpu_cal3 pu_cal[3] 0 SED_pucal3_SED
+* PULLDOWN
+vpd_cal0 pd_cal[0] 0 SED_pdcal0_SED
+vpd_cal1 pd_cal[1] 0 SED_pdcal1_SED
+vpd_cal2 pd_cal[2] 0 SED_pdcal2_SED
+vpd_cal3 pd_cal[3] 0 SED_pdcal3_SED
+
+* INPUT SIGNAL
+*          vlow, vhigh, delay, risetime, falltime, pulsewidth, period, phase
+vvddq vddq 0 0 PULSE 0   SED_vdd_SED 1n 10p 10p 5n 10n 0
+
+.control
+save all
+set temp=SED_temp_SED
+set num_threads=6
+
+* RUN SIMULATION
+tran 1p 8n
+* Measure rise time
+meas tran tdiff_charge trig dq val=0.1 rise=1 targ dq val=SED_vdd_SED*0.95 rise=1
+* Measure fall time
+meas tran tdiff_discharge trig dq val=SED_vdd_SED-0.1 fall=1 targ dq val=SED_vdd_SED*0.05 fall=1
+
+* OUTPUT
+wrdata ./out/SED_plotName_SED/SED_plotName_SED.txt tdiff_charge tdiff_discharge
+set hcopydevtype = svg
+hardcopy ./out/SED_plotName_SED/SED_plotName_SED.svg dq vddq title 'DQ vs time'
+
+.endc
+"}
+C {devices/code.sym} 930 -200 0 0 {name=MODELS
+only_toplevel=true
+format="tcleval( @value )"
+value="** Local library links to pdk
+.lib ./sky130/libs/SED_process_SED_lib.spice SED_process_SED
+.include \\\\$::SKYWATER_STDCELLS\\\\/sky130_fd_sc_hd.spice
+.include ./layout/pex_SSTL.spice
+"
+spice_ignore=false}
+C {devices/lab_wire.sym} 860 -330 2 0 {name=l9 sig_type=std_logic lab=DQ}
+C {devices/lab_wire.sym} 1000 -330 0 1 {name=l11 sig_type=std_logic lab=VDDQ}
+C {devices/lab_pin.sym} 350 -360 0 0 {name=l13 sig_type=std_logic lab=pu_cal[0:3]}
+C {devices/lab_pin.sym} 350 -300 0 0 {name=l14 sig_type=std_logic lab=pd_cal[0:3]}
+C {devices/res.sym} 950 -330 1 0 {name=Rtb
+value=25
+footprint=1206
+device=resistor
+m=1}
+C {schem/post_layout_SSTL.sym} 360 -210 0 0 {name=X1}
+C {devices/gnd.sym} 460 -210 0 0 {name=l19 lab=GND}
+C {devices/vdd.sym} 460 -450 0 0 {name=l20 lab=VDD}
+C {devices/gnd.sym} 240 -390 0 0 {name=l3 lab=GND}
+C {devices/gnd.sym} 240 -270 0 0 {name=l6 lab=GND}
---
+++ scripts/sim_sstl_cap.py
@@ -0,0 +1,113 @@
+from sys import argv
+from leg_sim_util import *
+import argparse
+from math import floor
+from numpy import arange
+from random import randrange
+import json
+from warnings import warn
+
+def get_valid_cal(json_fname, temp, vdd, proc):  
+  with open(json_fname, 'r') as f: j = json.loads(f.read())
+  arr = [val for i,val in enumerate(j) if val['temperature']==temp and val['vdd']==vdd and val['process']==proc]
+  if len(arr)<=0:
+    # return None
+    warn('No calibration fround for this corner, using default cal enum: 7')
+    return 7 # Return default cal enum so simulation runs anyway
+  assert (len(arr)<2), 'Multiple calibrations found for this corner!'
+  if (arr[0]['calibration_success']==False):
+    warn('Calibration failed for this corner! using MAX cal enum: 15')
+    return 15
+  return arr[0]['valid_cal_enum']
+
+def sim_slew(template, name, pu_cal, pd_cal, temp, vdd, proc):
+  replace_dict = {}
+  replace_dict['plotName'] = name
+  replace_dict['vdd']      = vdd
+  replace_dict['temp']     = temp
+  replace_dict['process']  = proc
+
+  for j in range(4): # 4 calibration fets for each leg
+    s = '0'
+    if cal2ctrl(pu_cal)[j]: s = str(vdd)
+    replace_dict['pucal{}'.format(j)] = s
+    s = '0'
+    if cal2ctrl(pd_cal)[j]: s = str(vdd)
+    replace_dict['pdcal{}'.format(j)] = s
+
+  gen_spice_script(template, name, replace_dict)
+
+  # Launch NGspice
+  try:
+    print('NGSPICE launched script "{outName}.spice"... '.format(outName=name))
+    os.system('''
+      make launch-ngspice \
+      args=out/{outName}/{outName}.spice\\ -b\\ -o\\ out/{outName}/{outName}.log \
+      >/dev/null 2>&1'''.format(outName=name))
+    print('NGSPICE complete.')
+    sleep(0.200) # 200 ms to leave time for interrupt
+  except KeyboardInterrupt:
+    print('\nSimulation halted')
+    quit()
+
+  # Read output
+  data = {}
+  with open('out/{name}/{name}.txt'.format(name=name), 'r') as d_file:
+    l = d_file.readline()
+    #data['slew_up']   = round(0.2*vdd / float(l.split()[1]) / 1e9, 2) # V/ns
+    #data['slew_down'] = round(0.2*vdd / float(l.split()[3]) / 1e9, 2) # V/ns
+    # charge_time = 3*25ohms*C
+    data['cap_charge']    = float(l.split()[1])/(3*25) / 1e12 # pF
+    data['cap_discharge'] = float(l.split()[1])/(3*25) / 1e12 # pF
+  return data
+
+
+def main():  
+  parser = argparse.ArgumentParser(
+    description='Generate and run a SSTL measurement simulation.')
+  parser.add_argument('--voltage', required=True, 
+    help='VDD voltage for the sim')
+  parser.add_argument('--temp', required=True, 
+    help='temperature (*C) for the sim')
+  parser.add_argument('--process', required=True, 
+    help='process string for the sim')
+  parser.add_argument('--post-layout', action='store_true',
+    help='Run post-layout simulation instead.')
+  args = parser.parse_args()
+
+  # Set names based on simulation type
+  tmp = '' # schematic sim not supported
+  outNameTmp = 'sstl_cap_{t}_{v}_{p}'
+  puCalTmp = './out/results/sstl_pu_{n}_resistance.json'
+  pdCalTmp = './out/results/sstl_pd_{n}_resistance.json'
+  if args.post_layout:
+    tmp = 'spice/post_layout_sstl_cap_tb.spice' # Post-layout spice script template
+    outNameTmp = 'post_layout_sstl_cap_{t}_{v}_{p}'
+    puCalTmp = './out/results/post_layout_sstl_pu_{n}_resistance.json'
+    pdCalTmp = './out/results/post_layout_sstl_pd_{n}_resistance.json'
+
+  outName = outNameTmp.format(t=args.temp, v=args.voltage, p=args.process)
+
+  pu_cal_file = puCalTmp.format(n=7) # Load the 7-leg calibration
+  pd_cal_file = pdCalTmp.format(n=7)
+  pu_cal = get_valid_cal(pu_cal_file, args.temp, args.voltage, args.process)
+  pd_cal = get_valid_cal(pd_cal_file, args.temp, args.voltage, args.process)
+  if pu_cal == None or pd_cal == None:
+    warn('No calibration for this corner! Skipping Slew sim')
+    return
+
+  result = sim_slew(tmp, outName, pu_cal, pd_cal, float(args.temp), float(args.voltage), args.process)
+
+  data_out = {}
+  data_out['vdd']         = args.voltage
+  data_out['temperature'] = args.temp
+  data_out['process']     = args.process
+  data_out['pu_cal_enum'] = pu_cal
+  data_out['pd_cal_enum'] = pd_cal
+  data_out.update(result) # Add slew results to dictionary
+  fout_name = 'out/{name}/out.json'.format(name=outName)
+  with open(fout_name, 'w') as f: f.write(json.dumps(data_out, indent=2))
+
+
+if __name__ == '__main__':
+  main()
